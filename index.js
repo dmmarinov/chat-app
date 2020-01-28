d79574ba239e5f7f3b818462885f50376e0787ba
Dmitry Marinov <admin@milled.ru>
2020-01-29 01:10:23 +0400

    Simple chat using websockets

---
+++ index.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({
+  port: process.env.PORT || 5000
+});
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    wss.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
